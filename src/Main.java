38a3e07ffa6e584741b50ac1b2cec56638da495c
Sharkeee <sharkmen1996@gmail.com>
2017-12-22 12:13:42 +0100

    Warunki sprawdzajace poprawnosc wprowadzonych danych

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
 
-    static int liczbaKolumn = Integer.parseInt(JOptionPane.showInputDialog(null, "[Tworzenie Tabeli] Wporwadz liczbe kolumn: "));
+    static int liczbaKolumn = Integer.parseInt(JOptionPane.showInputDialog(null, "[Tworzenie Tabeli] Wprowadz liczbe kolumn: "));
     static int liczbaWierszy = Integer.parseInt(JOptionPane.showInputDialog(null, "[Tworzenie tabeli] Wprowadz liczbe wierszy: "));
     static int convertedColumn = 0;
     static String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "W", "X", "Y", "Z"};
@@ -10,8 +10,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        if (liczbaKolumn < 0 || liczbaKolumn > letters.length) {
+            System.out.println("Liczba kolumn wykracza poza rozmiar alfabetu. Maksymalna liczba kolumn do utworzenia to: " + letters.length);
+        }
+        if (liczbaWierszy < 0 || liczbaWierszy > 9999) {
+            System.out.println("Niepoprawna liczba wierszy, wprowadz liczbe z zakresu (0-9999");
+        }
 
-        System.out.println("Wygenerowano tablice zawierajaca kolumny (" + liczbaKolumn + ") oraz wiersze(" + liczbaWierszy + ")");
         sumujKomorki();
     }
 
@@ -24,11 +29,13 @@ public class Main {
         }
         int wyborKomorki = Integer.parseInt(JOptionPane.showInputDialog(null, "Wybierz zakres komorek do zsumowania (1-" + liczbaWierszy + ")"));
 
-        if (wyborKomorki > liczbaWierszy || (convertedColumn - 1) > liczbaKolumn) {
+        if (wyborKomorki > liczbaWierszy || wyborKomorki < 0 || (convertedColumn - 1) > liczbaKolumn || (convertedColumn - 1) < 0) {
             System.out.println("Nie udalo sie zsumowac wybranego zakresu komorek. Prawdopodobnie wykroczyles poza granice utworzonej tabeli.");
         } else {
-            System.out.println("\n\nOgolny zakres tabeli:\n Stworzone kolumny(" + letters[0] + " - " + letters[liczbaKolumn - 1] + "). \n Stworzone wiersze: (" + liczbaWierszy + ")");
-            System.out.println("Laczna liczba komorek w wybranym przez Ciebie zakresie to: " + (convertedColumn * wyborKomorki));
+            System.out.println("\n\nParametry wygenerowanej tabeli:\n Stworzone kolumny(" + letters[0] + " - " + letters[liczbaKolumn - 1] + "). \n Stworzone wiersze: (0-" + liczbaWierszy + ")");
+            System.out.println("\nLaczna liczba komorek w wybranym przez Ciebie zakresie to: " + (convertedColumn * wyborKomorki));
         }
     }
+
+    
 }
